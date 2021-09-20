6eb7f078bd49bc31898bdbc316514cf59d1c28a0
Huy Nguyen <huyite.vn@gmail.com>
2021-09-20 23:03:41 +0700

    update

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -8,6 +8,7 @@ struct Element{
 struct Queue{
     Element * Head;
     Element * Tail;
+    int number;
 };
 Element* CreateElement(int data){
     Element *e=new Element;
@@ -15,10 +16,38 @@ Element* CreateElement(int data){
     e->next=NULL;
     return e;
 }
-void InitQueue(Queue& S){
-    S.head=S.Tail=NULL;
+void InitQueue(Queue& q){
+    q.head=q.Tail=NULL;
+    q.number=0;
 }
+void Enqueue(Element *e,Queue &q){
+if(q.head==NULL)
+    q.head=q.tail=e;
+   else{
+    e->pointer=q.head;
+    q.head=e;
+   }
+   q.number++;
+}
+bool isEmpty(Queue q){
+    if(q.number>0)
+        return true;
+    else false;
+}
+Element * Dequeue(Queue &q){
+    if(q.tail!=NULL){
+     Element *p=q.head;
 
+     while(p->next!=q.tail)
+        p=p->next;
+
+     Element *tmp=q.tail;
+     q.tail=p;
+     p->next=NULL;
+     delete tmp;
+
+     }
+}
 
 int main()
 {
--- Stack/main.cpp
+++ Stack/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 struct Element{
     int data;
     Element* next;
+    int number;
 };
 struct Stack{
     Element * Head;
+    int number;
 };
 Element* CreateElement(int data){
     Element *e=new Element;
@@ -16,6 +18,7 @@ Element* CreateElement(int data){
 }
 void InitStack(Stack& S){
     S.head=NULL;
+    S.number=0
 }
 void push(Element *e,Stack& S){
   if(S.head==NULL)
@@ -24,13 +27,21 @@ void push(Element *e,Stack& S){
     e->pointer=S.head;
     S.head=e;
    }
+   S.number++;
+}
+bool isEmpty(Stack S){
+    if(S.number>0)
+        return true;
+    else false;
 }
 Element* pop(Stack&S){
    if(S.head!=NULL){
-    Element *p=L.head;
-    L.head=L.head->pointer;
+    Element *p=S.head;
+    S.head=S.head->pointer;
+    S.number--;
     return p;
     }
+    else return NULL;
 }
 void TravelStack(Stack L){
      Element *p=L.head;
